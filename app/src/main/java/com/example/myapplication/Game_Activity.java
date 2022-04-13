ee0d26e24b907ced67fffa1144b1b52f943b0c99
ASUS <mostafa25509576@gmail.com>
2022-04-13 22:46:24 +0300

    fares and mustafa2

--- app/src/main/java/com/example/myapplication/Game_Activity.java
+++ app/src/main/java/com/example/myapplication/Game_Activity.java
@@ -10,5 +10,7 @@ public class Game_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // fares and mustafa
     }
 }
